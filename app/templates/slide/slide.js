6b936e32c936db8c882e20fd5f45711cfbc24024
Jberryliu <liuyz0602@163.com>
2015-03-16 18:15:52 +0800

    add commit

--- app/templates/slide/slide.js
+++ app/templates/slide/slide.js
@@ -14,15 +14,19 @@ define('slide<%= slideIndex %>', [], function (require) {
 
         },
 
+        /*
+            @desc: 滑动完成 后 回调函数
+            @param: 当前可见 slide
+        */
         before: function ($slide) {
 
         },
 
-        after: function () {
-
-        },
-
-        progress: function () {
+        /*
+            @desc: 滑动完成 后 回调函数
+            @param: 当前可见 slide
+         */
+        after: function ($slide) {
 
         }
 
